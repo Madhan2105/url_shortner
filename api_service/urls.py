f80815b064f57d03b03ec9e8955ed1edbbddd7c5
Madhan2105 <nadarmadhan43@gmail.com>
2020-09-16 20:32:46 +0530

    url changes

--- api_service/urls.py
+++ api_service/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.url_shortener, name='url_shortener'),
     path('api/', views.url_shortener, name='url_shortener'),
     path('api/<str:optional_slug>', views.url_shortener, name='url_shortener_get'),
     path('goto/<slug:slug>/', views.redirect_site),
